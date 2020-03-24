4d299c1b9559589b2011a0192be0a8a86ba31722
Boss <boss@staff.libraryofcode.us>
2020-03-23 23:46:22 -0400

    added the first few dyno bot commands

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Eva 
 Eva is just a utility eval bot, that has some use for Dyno Bot Support via a few commands that only the staff team normally have access to. They are: 
-- `!prod [serverid]` (shows the shard/cluster information for a guild)
+~~- `!prod [serverid]` (shows the shard/cluster information for a guild)~~
 - `!cluster [cluster number]` (shows specific information on the cluster provided)
 - `!status` (lists the status of each dyno server)
 - `!server [server name]` (lists specific information for the provided Dyno server)
-- `!prem [server id]` (shows the shard/cluster information for a premium guild)
+~~- `!prem [server id]` (shows the shard/cluster information for a premium guild)~~
 - `!premstatus` (shows the status of premium)
 
 ## Contributing 
---
+++ commands/dyno/prem.js
@@ -0,0 +1,41 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const config = require('../../config.json')
+const axios = require('axios'); 
+
+module.exports = {
+    name: "prem",
+    category: "dyno",
+    description: "Tells you the shard and cluster that a Dyno Premium server is on.",
+    run: async (client, message, args) => {
+        if(!config.whitelisted.includes(message.author.id)) return; 
+
+        const getStatus = async () => { 
+            try { 
+                return await axios.get('https://dyno.gg/api/status')
+            } catch (error) { 
+                console.error(error)
+            }
+        }
+        
+        const status = await getStatus()
+      
+        if (!args.join(' ')) return message.channel.send('Provide a server ID!')
+        const cluster = Math.floor(((args.join(' ')/ 4194304) % 20)/1)
+        const shard = Math.floor((args.join(' ')/ 4194304) % 20)
+        const cdata = status.data.premium.statuses[cluster].result
+
+        const embed = new RichEmbed()
+        .setTitle('Dyno Cluster/Shard Information')
+        .addField('Server ID', args.join(' '))
+        .addField('Server', `${cdata.server}`)
+        .addField('Cluster', cluster)
+        .addField('Shard', shard)
+        .setColor('BLUE')
+        .setFooter(`Uptime: ${cdata.uptime}`)
+        .setTimestamp()
+
+        message.channel.send(embed)
+    
+    }
+}
