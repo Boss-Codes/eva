d4552cd5ec5090456bde7e331076428efbc6381b
Boss <boss@staff.libraryofcode.us>
2020-03-29 18:46:52 -0400

    reverted

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ const { RichEmbed } = require('discord.js');
 
 module.exports = async (client, message) => {
 
-console.log(`I am online as ${client.user.tag} in ${client.user.guilds.size} guilds and for ${client.user.users.size} users!`)
+console.log(`I am online as ${client.user.tag} in ${client.guilds.size} guilds and for ${client.users.size} users!`)
 client.user.setPresence({
  status: 'online', 
  game: { 
